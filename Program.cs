e0446f28abac8f409d8e24409b17aafe66d0826e
AhmedShaabanAl-Saidi <ahmedshaaban123321@gmail.com>
2025-01-13 11:49:09 +0200

    StoreSellProducts

--- Program.cs
+++ Program.cs
@@ -2,9 +2,22 @@
 {
     internal class Program
     {
+        #region StoreSellProducts Function
+        public static void StoreSellProducts(int numOfBooks = 0, int numOfElectronics = 0, int numOfClothing = 0)
+        {
+            Console.WriteLine("Welcome in Store Sell Product App :");
+
+            decimal booksPrice = 24.99m;
+            decimal electronicsPrice = 149.95m;
+            decimal clothingPrice = 39.50m;
+
+            Console.WriteLine($"Total price of : \"{numOfBooks} Books , {numOfElectronics} Electronics , {numOfClothing} Clothing\" = {(numOfBooks * booksPrice) + (numOfElectronics * electronicsPrice) + (numOfClothing * clothingPrice)}$");
+        } 
+        #endregion
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            StoreSellProducts(1,1,1);
         }
     }
 }

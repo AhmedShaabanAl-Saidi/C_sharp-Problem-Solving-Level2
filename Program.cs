1ede4b2c5d48c3ae14ab6a38a1774d8cbd28fd3c
AhmedShaabanAl-Saidi <ahmedshaaban123321@gmail.com>
2025-01-13 13:29:11 +0200

    BuyOneGetHalf

--- Program.cs
+++ Program.cs
@@ -21,6 +21,23 @@
             Console.WriteLine("Welcome in Weekend Customers App :");
 
             Console.WriteLine($"Store Serves in Weekend More 10% Customers = {storeServesNums + (storeServesNums * 0.10)}");
+        }
+        #endregion
+
+        #region BuyOneGetHalf Function
+        public static void BuyOneGetHalf(int numOfPurchase)
+        {
+            Console.WriteLine("Welcome in Buy One Get Half App :");
+
+            decimal tshirtPrice = 32m;
+
+            decimal boundelPrice = tshirtPrice + (tshirtPrice * 0.5m);
+            int boundelCount = numOfPurchase / 2;
+
+            if (numOfPurchase % 2 == 0)
+                Console.WriteLine($"Price of {numOfPurchase} Tshirts = {boundelPrice * boundelCount}$");
+            else
+                Console.WriteLine($"Price of {numOfPurchase} Tshirts = {(boundelPrice * boundelCount) + tshirtPrice}$");
         } 
         #endregion
 
@@ -29,6 +46,8 @@
             //StoreSellProducts(1,1,1);
 
             //WeekendCustomers(120);
+
+            //BuyOneGetHalf(5);
         }
     }
 }

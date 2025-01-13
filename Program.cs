5487297a368c3f616573203f89710a036144540e
AhmedShaabanAl-Saidi <ahmedshaaban123321@gmail.com>
2025-01-13 11:14:04 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace C_sharp_Problem_Solving_Level2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
